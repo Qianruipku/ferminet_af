16dbc15713529318a29ef2f5c71a34fcd4c8b580
Andres Perez Fadon <aperezfa@login07.leonardo.local>
2024-11-08 12:20:41 +0100

    Made example more explicit

--- ferminet/tests/pbc_test.py
+++ ferminet/tests/pbc_test.py
@@ -7,30 +7,38 @@ from ferminet.constants import MUON_MASS, PROTON_MASS
 import jax.numpy as jnp
 import sys
 
+
+# 14-electron unpolarised homogeneous electron gas with rs = 5
+
+rs = 5.0
+N = 14
+
+L = rs * (4 * jnp.pi * N / 3)**(1/3)
+
 def get_config():
   # Get default options.
   cfg = base_config.default()
 
   # Set up molecule
-  cfg.system.particles = (2, 2)
+  cfg.system.particles = (N // 2, N // 2)
   cfg.system.charges = (-1., -1.)
   cfg.system.masses = (1., 1.)
   cfg.system.molecule = [system.Atom('X', (0, 0, 0))]
   
   # Periodic boundary conditions
   cfg.system.pbc.apply_pbc = True
-  cfg.system.pbc.lattice_vectors = jnp.eye(3)
+  cfg.system.pbc.lattice_vectors = jnp.eye(3) * L
   cfg.system.pbc.convergence_radius = 5
   cfg.system.pbc.min_kpoints = None
 
   # Set training hyperparameters
   cfg.batch_size = 4096
   cfg.pretrain.iterations = 0
-  cfg.optim.iterations = 1000 + 100
+  cfg.optim.iterations = 40_000 + 10
 
   cfg.optim.laplacian = "folx"
 
-  cfg.log.save_frequency = 5000
+  cfg.log.save_frequency = 40_000
   cfg.log.save_path = "checkpoint_training"
   cfg.log.restore_path = "checkpoint_training"
 
